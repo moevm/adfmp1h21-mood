b32ee213ed4a8d30565ed6ff778b13d1c5879195
alskaa <alisagorodeckaa7786@gmail.com>
2021-03-29 18:11:04 +0300

    add custom variants

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
 import android.view.ViewConfiguration
 import android.view.ViewGroup
@@ -35,7 +36,8 @@ class MainActivity : AppCompatActivity() {
                 "Грустное",
                 "Ужасное",
                 "Задумчивое",
-                "Мечтательное"
+                "Мечтательное",
+                "Свой вариант..."
         )
 
         val moodData = mutableListOf<String>()
@@ -99,6 +101,11 @@ class MainActivity : AppCompatActivity() {
             ) {
                 if (position != 0) {
                     val item = parent!!.getItemAtPosition(position).toString()
+                    if (item == "Свой вариант...") {
+                        parent.visibility = View.GONE
+                        moodedit.visibility = View.VISIBLE
+                        return
+                    }
                     if (!moodData.contains(item)) {
                         moodData.add(item)
                         moodAdapter.notifyDataSetChanged()
@@ -114,6 +121,35 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        moodedit.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+                if (event.getAction() == KeyEvent.ACTION_DOWN &&
+                    keyCode == KeyEvent.KEYCODE_ENTER
+                ) {
+                    val item = moodedit.text.toString()
+                    if (!moodData.contains(item)) {
+                        moodData.add(item)
+                        moodAdapter.notifyDataSetChanged()
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Настроение добавлено",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        Toast.makeText(this@MainActivity, "Такое настроение уже добавлено",Toast.LENGTH_LONG).show()
+                    }
+                    moodedit.visibility = View.GONE
+                    moodspinner.visibility = View.VISIBLE
+                    moodedit.text.clear()
+                    moodspinner.setSelection(0)
+                    return true
+                }
+                return false
+            }
+        })
+        moodedit.setHintTextColor(Color.GRAY)
+
         //
         val states = arrayOf(
                 "Я чувствую себя...",
@@ -124,7 +160,8 @@ class MainActivity : AppCompatActivity() {
                 "Ужасно",
                 "Болезненно",
                 "Сонно",
-                "Бодро"
+                "Бодро",
+                "Свой вариант..."
         )
 
         val stateData = mutableListOf<String>()
@@ -188,6 +225,11 @@ class MainActivity : AppCompatActivity() {
             ) {
                 if (position != 0) {
                     val item = parent!!.getItemAtPosition(position).toString()
+                    if (item == "Свой вариант...") {
+                        parent.visibility = View.GONE
+                        stateedit.visibility = View.VISIBLE
+                        return
+                    }
                     if (!stateData.contains(item)) {
                         stateData.add(item)
                         stateAdapter.notifyDataSetChanged()
@@ -204,6 +246,35 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        stateedit.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+                if (event.getAction() == KeyEvent.ACTION_DOWN &&
+                    keyCode == KeyEvent.KEYCODE_ENTER
+                ) {
+                    val item = stateedit.text.toString()
+                    if (!stateData.contains(item)) {
+                        stateData.add(item)
+                        stateAdapter.notifyDataSetChanged()
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Самочувствие добавлено",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        Toast.makeText(this@MainActivity, "Такое самочувствие уже добавлено",Toast.LENGTH_LONG).show()
+                    }
+                    stateedit.visibility = View.GONE
+                    statespinner.visibility = View.VISIBLE
+                    stateedit.text.clear()
+                    statespinner.setSelection(0)
+                    return true
+                }
+                return false
+            }
+        })
+        stateedit.setHintTextColor(Color.GRAY)
+
+
         val doingData = mutableListOf<String>()
         val doingAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, doingData)
         var mDoingLastClickTime = 0L
@@ -212,8 +283,8 @@ class MainActivity : AppCompatActivity() {
             val currTime = System.currentTimeMillis()
             if (mDoingLastClickTime != 0L) {
                 if (currTime - mDoingLastClickTime < ViewConfiguration.getDoubleTapTimeout()) {
-                    stateData.removeAt(position)
-                    stateAdapter.notifyDataSetChanged()
+                    doingData.removeAt(position)
+                    doingAdapter.notifyDataSetChanged()
                 }
             }
             mDoingLastClickTime = currTime
@@ -225,7 +296,7 @@ class MainActivity : AppCompatActivity() {
         val times = arrayOf(
                 "Время", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00",
                 "16:00", "17:00", "18:00", "19:00", "20:00", "21:00","22:00", "23:00", "00:00", "1:00", "2:00",
-                "3:00", "4:00", "5:00", "6:00"
+                "3:00", "4:00", "5:00", "6:00", "Свой вариант..."
         )
 
         val timeSpinnerAdapter: ArrayAdapter<String> = object : ArrayAdapter<String>(
@@ -274,8 +345,14 @@ class MainActivity : AppCompatActivity() {
             ) {
                 if (position != 0) {
                     val item = parent!!.getItemAtPosition(position).toString()
+                    if (item == "Свой вариант...") {
+                        parent.visibility = View.GONE
+                        timeedit.visibility = View.VISIBLE
+                        timeedit.isActivated = true
+                        return
+                    }
                     if (doing != null) {
-                        val text = "$item $doing"
+                        val text = "$item. $doing"
                         if (!doingData.contains(text)) {
                             doingData.add(text)
                             doingAdapter.notifyDataSetChanged()
@@ -302,9 +379,53 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        timeedit.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+                if (event.getAction() == KeyEvent.ACTION_DOWN &&
+                    keyCode == KeyEvent.KEYCODE_ENTER
+                ) {
+                    val item = timeedit.text.toString()
+                    if (doing != null) {
+                        val text = "$item. $doing"
+                        if (!doingData.contains(text)) {
+                            doingData.add(text)
+                            doingAdapter.notifyDataSetChanged()
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Занятие добавлено",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Такое действие уже добавлено",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                        timespinner.setSelection(0)
+                        doingspinner.setSelection(0)
+                        time = null
+                        doing = null
+                        timeedit.visibility = View.GONE
+                        timespinner.visibility = View.VISIBLE
+                        doingedit.visibility = View.GONE
+                        doingspinner.visibility = View.VISIBLE
+                        timeedit.text.clear()
+                        doingedit.text.clear()
+                    } else {
+                        time = item
+                        timeedit.isEnabled = false
+                    }
+                    return true
+                }
+                return false
+            }
+        })
+        stateedit.setHintTextColor(Color.GRAY)
+
         val doings = arrayOf(
                 "Занятие", "Работа", "Учеба", "Завтрак", "Второй завтрак", "Обед", "Ужин", "Сон",
-                "Разглядывание стены")
+                "Разглядывание стены", "Свой вариант...")
 
         val doingSpinnerAdapter: ArrayAdapter<String> = object : ArrayAdapter<String>(
             this,
@@ -352,8 +473,14 @@ class MainActivity : AppCompatActivity() {
             ) {
                 if (position != 0) {
                     val item = parent!!.getItemAtPosition(position).toString()
+                    if (item == "Свой вариант...") {
+                        parent.visibility = View.GONE
+                        doingedit.visibility = View.VISIBLE
+                        doingedit.isActivated = true
+                        return
+                    }
                     if (time != null) {
-                        val text = "$time $item"
+                        val text = "$time. $item"
                         if (!doingData.contains(text)) {
                             doingData.add(text)
                             doingAdapter.notifyDataSetChanged()
@@ -379,6 +506,50 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        doingedit.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+                if (event.getAction() == KeyEvent.ACTION_DOWN &&
+                    keyCode == KeyEvent.KEYCODE_ENTER
+                ) {
+                    val item = doingedit.text.toString()
+                    if (time != null) {
+                        val text = "$time. $item"
+                        if (!doingData.contains(text)) {
+                            doingData.add(text)
+                            doingAdapter.notifyDataSetChanged()
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Занятие добавлено",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Такое действие уже добавлено",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                        timespinner.setSelection(0)
+                        doingspinner.setSelection(0)
+                        time = null
+                        doing = null
+                        timeedit.visibility = View.GONE
+                        timespinner.visibility = View.VISIBLE
+                        doingedit.visibility = View.GONE
+                        doingspinner.visibility = View.VISIBLE
+                        timeedit.text.clear()
+                        doingedit.text.clear()
+                    } else {
+                        doing = item
+                        doingedit.isEnabled = false
+                    }
+                    return true
+                }
+                return false
+            }
+        })
+        doingedit.setHintTextColor(Color.GRAY)
 
         // go to calendar
         btn_calendar.setOnClickListener {
